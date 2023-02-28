0c977271f8e71898f8798729bb1e96530e7b4e16
fuatgormez <fuatgormez@me.com>
2023-02-28 22:23:45 +0100

    product name rules

--- AppFG/controllers/shop/Cart.php
+++ AppFG/controllers/shop/Cart.php
@@ -150,6 +150,7 @@ class Cart extends CI_Controller
 
                         $this->cart->product_name_rules = '[:print:]';
                         $this->cart->insert($product_data);
+                        
                         //qty fazla gelirse tekrardan kontrol edip miktari 1 e set ediyoruz buraya sonradan tekrar bak
                         $this->check_gift_product_no_price();
 
