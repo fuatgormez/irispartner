d002c5ac6a4364276637abe5203a3e5218871d43
fuatgormez <fuatgormez@me.com>
2023-02-28 14:33:43 +0100

    first commit

---
+++ AppFG/views/layout/porto/view_partner.php
@@ -0,0 +1,209 @@
+<!--==============================
+Hero Area
+============================== -->
+
+<section class="vs-hero-wrapper">
+
+</section>
+
+
+<!--==============================
+Hero Area End
+============================== -->
+
+<div class="background-image" data-vs-img="<?php echo base_url(); ?>public/layout/<?php echo $theme; ?>/img/bg-img/bg-img-3-1.png">
+    <!--==============================
+    Services Area
+============================== -->
+    <section class="vs-service-wrapper service-box-layout3 pt-5 pt-md-3 pt-lg-5 pt-xl-0 pb-30 pb-lg-100 mt-sm-5" id="service">
+        <div class="container">
+            <div class="row justify-content-center text-center">
+                <div class="col-md-12">
+                    <div class="section-title3">
+                        <h3 class="title3">IRIS PHOTO BOX👁iris photography</h3>
+                        <p><img src="<?php echo base_url('public/uploads/partner-01.jpg'); ?>"></p>
+                        <iframe width="760" height="274" src="https://www.youtube.com/embed/FGSs1KEDdKU" title="IRIS PARTNER" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                        <p><a href="<?php base_url('contact');?>" class="btn btn-success">Become a Partner</a></p>
+
+                        <h3 class="title3">INFO PARTNERSHIP</h3>
+                        <p>Put iris box in your OFFICE, SHOP, HOME and wait for us to send you customers.</p>
+                        <p>Then we can definitely work together very well! IRIS PARTNER offers a way to earn money with a franchise structure of 0 € without the need for additional time.</p>
+                        <h3 class="title3">How does it work?</h3>
+                        <p>1. We will send Iris Box to you.</p>
+                        <p>2. We promote our products in a variety of settings.</p>
+                        <p>3. We adapt to you! Just let us know what days and times you will be shooting.</p>
+                        <p>4. Our responsible department after the photo shoot is finished It does the photoshop process and transfers it to production.</p>
+                        <p>5. Iris Partner is responsible for sending the desired finished product to the customer.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center text-center">
+                <hr>
+                <div class="col-md-6">
+                    <div class="section-title3">
+                        <iframe width="425" height="239" src="https://www.youtube.com/embed/Rr1ieRPW9vM" title="irisbox" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="section-title3">
+                        <iframe width="425" height="239" src="https://www.youtube.com/embed/Rr1ieRPW9vM" title="irisbox" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            <div class="row justify-content-center text-center">
+                <div class="col-md-12">
+                    <div class="section-title3">
+                    <p>Innovation has been at the heart of our company since 2019. We are not only pioneers but also inventors of artistic iris photography.</p>
+                    <h3 class="title3">Do you have a job?</h3>
+                    <p>Hairdresser, optician, coffee shop, beauty center, hotel, clothing store, telephone store, gift shop, etc. No matter what industry you are in, iris photography appeals to everyone.
+                    <h3 class="title3">Don't you have a job?</h3>
+                    <p>You can shoot with the 40x40 cm Iris Photo Box at your home.</p>
+                    <hr>
+                    <p>You just take the iris photo, 1 minute, leave the rest to us</p>
+                    <iframe width="425" height="239" src="https://www.youtube.com/embed/kRjQnLshCRc" title="PHOTOSHOP OFFICE" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <hr>
+                    <h3 class="title3">We send the desired finished product to the customer</h3>
+                    <iframe width="425" height="239" src="https://www.youtube.com/embed/oHtaZFKQ0k0" title="Production" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center text-center">
+                <div class="col-md-4">
+                    <div class="section-title3">
+                        <iframe width="278" height="157" src="https://www.youtube.com/embed/Y0w9g24EZuI" title="Partner Locations" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="section-title3">
+                        <iframe width="278" height="157" src="https://www.youtube.com/embed/rAnJ23he2tw" title="Acrylic" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="section-title3">
+                        <iframe width="278" height="157" src="https://www.youtube.com/embed/OnT-8d7xxLQ" title="Glass for Couple" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="section-title3">
+                        <iframe width="278" height="157" src="https://www.youtube.com/embed/LSZeY2URF4M" title="Glass for Family" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="section-title3">
+                        <iframe width="278" height="157" src="https://www.youtube.com/embed/EoKfSvlGd3M" title="Canvas" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="section-title3">
+                        <iframe width="278" height="157" src="https://www.youtube.com/embed/fImFB4rqGsE" title="Necklace" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--==============================
+Services Area End
+============================== -->
+</div>
+<!--==============================
+Contact Form Area
+============================== -->
+
+<section class="vs-contact-form contact-form-layou1 position-relative py-10 py-lg-20" id="partner">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-xl-12 justify-content-center text-center">
+                <?php if ($this->session->flashdata('error')) : ?>
+                    <div class="alert alert-danger"><?php echo $this->session->flashdata('error'); ?></div>
+                <?php endif; ?>
+                <?php if ($this->session->flashdata('success')) : ?>
+                    <div class="alert alert-success"><?php echo $this->session->flashdata('success'); ?></div>
+                <?php endif; ?>
+            </div>
+            <div class="col-lg-12 col-xl-12 justify-content-center text-center mt-5">
+                    <h3 class="title3">I want to be a partner</h3>
+            </div>
+            <div class="col-lg-12 col-xl-12 mt-3">
+                <?php echo form_open(base_url('partner/send'), array('class' => 'contact-form input-style3')); ?>
+                <div class="row gutters-10">
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Name" name="firstname" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Vorname" name="lastname" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Email" name="email" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Tel" name="phone" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Land" name="land" required>
+                    </div>
+                    <div class="form-group col-md-6">
+                        <input type="text" class="form-control" placeholder="Stückzahl" name="qty" required>
+                    </div>
+                    <div class="form-group col-12">
+                        <textarea class="form-control" placeholder="Anmerkung" name="message"></textarea>
+                        <i class="fal fa-pencil-alt"></i>
+                    </div>
+                    <div class="form-group mb-0 col-12 mt-20">
+                        <button type="submit" class="btn btn-primary style3 rounded-0" name="form_contact"><?php echo $this->lang->line('contact_form_send_button'); ?> <i class="far fa-angle-right"></i></button>
+                    </div>
+                </div>
+                <p class="form-messages mt-20"></p>
+                <?php echo form_close(); ?>
+            </div>
+        </div><!-- first row end -->
+
+    </div>
+</section>
+
+<!--==============================
+Contact Form Area
+============================== -->
+
+
+<!-- Modal -->
+<div class="modal fade" id="franchise" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <div class="cart-table table-responsive">
+                    <h1>Uber Irispicture</h1>
+                    <p>Wir freuen uns, dass Du Dich für unsere einzigartigen Produkte der Iris-Fotografie interessierst und wir versprechen Dir: Wir kreieren ein individuelles Kunstwerk mit Deiner Iris. Eines, das es kein zweites Mal auf der Welt gibt!</p>
+
+
+
+                    <p>Sind deine Augen braun oder blau, sind sie grün oder grau? Ganz egal, auf jeden Fall sind sie
+
+                        wirklich einzigartig. Mit unser Technik der Iris-Fotografie verdeutlichen wir diese Einzigartigkeit. Wir
+
+                        machen ein spektakuläres, detailreiches Kunstwerk aus Deiner Iris. Die natürliche Schönheit Deiner Iris wird sichtbar. Es lohnt sich, dieses Unikat zu präsentieren und auch zu verschenken.</p>
+
+
+
+
+
+                    <h1>So funktioniert die Iris-Fotografie bei Iris Picture!</h1>
+                    <p>Die Iris des menschlichen Auges, auch Regenbogenhaut genannt, hat einen Durchmesser von etwa einem Zentimeter. Die durch Pigmente gefärbte Blende des Auges ist noch individueller als ein Fingerabdruck. Um die Iris zu fotografieren, braucht man ein spezielles Equipment und viel Erfahrung, welches Irispicture den Kunden mit voller Überzeugung bietet. Mit einer Kombination aus einem patentierten, selbst entwickeltem System mit künstlicher Intelligenz, Profi-Kamera, Makroobjektiv und einer Software zur späteren Bearbeitung der Aufnahme holen wir das Beste aus Deiner Iris heraus. Die einzigartige Schönheit Deiner individuellen Iris wird nämlich erst durch die Iris-Fotografie sichtbar. Unsere professionellen Grafiker bringen die Besonderheiten jeder Iris zur Geltung, denn jede Iris ist einzigartig und einem Feinschliff Wert.</p>
+
+
+
+                    <h1>100 % Kundenzufriedenheit!</h1>
+                    <p>Wir verknüpfen unsere 28- jährige Erfahrung in der Fotobranche mit der einzigartigen Iris- Kunst. Der langjährige Kundenkontakt bietet uns den Grundbaustein für kundenorientierte Arbeit. Wir legen großen Wert auf Kundenzufriedenheit und zufrieden stellender Service. Unser Hauptsitz und auch die Produktionsstätte liegt in Deutschland.</p>
+
+
+
+
+
+                    <h1 mt-5>Dein Foto, deine Iris!</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Modal End-->
